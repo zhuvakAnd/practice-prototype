3fc9a134776d29006e7a5018b04e7276901ddb86
A Z <zhuvak.andrii@gmail.com>
2024-04-28 15:13:58 +0300

    add models

---
+++ Models/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace practice_prototype.Models;
+
+public class Ticket
+{
+    public int Id { get; set; }
+    public int ScreenId { get; set; }
+    public int Seats { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace practice_prototype.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public ICollection<Ticket>? Tickets { get; set; }
+}
